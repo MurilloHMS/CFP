8edf73146152fbc9173564d7ef194c26617443e3
MurilloHMS <murillohenriquemelo@outlook.com>
2024-02-11 16:54:57 -0300

    inicializado configuração da recuperação

--- ControleFinanceiroPessoal/Model/Email.cs
+++ ControleFinanceiroPessoal/Model/Email.cs
@@ -10,6 +10,33 @@ namespace ControleFinanceiroPessoal.Model
 {
     internal class Email
     {
-        
+        static string GerarCodigo()
+        {
+            return new Random().Next(1000, 9999).ToString();
+        }
+
+        static void EnviarEmail(string servidorSmtp, int portaSmtp, string usuarioSmtp, string senhaSmtp, string destinatario, string assunto, string corpo)
+        {
+            SmtpClient clienteSmtp = new SmtpClient(servidorSmtp)
+            {
+                Port = portaSmtp,
+                Credentials = new NetworkCredential(usuarioSmtp, senhaSmtp),
+                EnableSsl = true,
+            };
+
+            // Configuração da mensagem
+            MailMessage mensagem = new MailMessage
+            {
+                From = new MailAddress(usuarioSmtp),
+                Subject = assunto,
+                Body = corpo,
+                IsBodyHtml = false,
+            };
+
+            mensagem.To.Add(destinatario);
+
+            // Envio da mensagem
+            clienteSmtp.Send(mensagem);
+        }
     }
 }
